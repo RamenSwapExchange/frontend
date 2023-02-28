a0cb41c4ae19de96abee6d74567e94507ddf8a7c
grekuu <91375853+grekuu@users.noreply.github.com>
2023-02-28 08:18:05 +0100

    init header commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,24 @@
 import "./header.scss";
+import { MdOutlineRamenDining } from "react-icons/md"
+import { BiDotsHorizontalRounded } from "react-icons/bi"
 
 const Header = () => {
-  return <div>Header</div>;
+  return <div className="header">
+    <div className="header-left">
+      <div className="logo"><MdOutlineRamenDining /></div>
+      <a>Swap</a>
+      <a>Tokens</a>
+      <a>Pool</a>
+    </div>
+    <div className="header-mid">
+      <input type="text" />
+    </div>
+    <div className="header-right">
+      <div className="dots-icon"><BiDotsHorizontalRounded /></div>
+      <div className="dropdown">Ethereum</div>
+      <button>Connect</button>
+    </div>
+  </div>;
 };
 
 export default Header;
