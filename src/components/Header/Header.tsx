154576314aa95e2a2ddf59ea99939f4ccf608ba1
grekuu <gracjanprusik@gmail.com>
2023-02-28 19:14:57 +0100

    initial styling finished

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,32 @@
 import "./header.scss";
-import { MdOutlineRamenDining } from "react-icons/md"
-import { BiDotsHorizontalRounded } from "react-icons/bi"
+import { MdOutlineRamenDining } from "react-icons/md";
+import { BiDotsHorizontalRounded } from "react-icons/bi";
+import { FaEthereum } from "react-icons/fa";
 
 const Header = () => {
-  return <div className="header">
-    <div className="header-left">
-      <div className="logo"><MdOutlineRamenDining /></div>
-      <a>Swap</a>
-      <a>Tokens</a>
-      <a>Pool</a>
+  return (
+    <div className="header">
+      <div className="header-left">
+        <div className="logo">
+          <MdOutlineRamenDining />
+        </div>
+        <a>Swap</a>
+        <a>Tokens</a>
+        <a>Pool</a>
+      </div>
+      <div className="header-mid">
+        <input type="text" placeholder="Search tokens and NFT collections" />
+      </div>
+      <div className="header-right">
+        <BiDotsHorizontalRounded className="dots-icon" />
+        <div className="dropdown">
+          <FaEthereum />
+          Ethereum
+        </div>
+        <button>Connect</button>
+      </div>
     </div>
-    <div className="header-mid">
-      <input type="text" />
-    </div>
-    <div className="header-right">
-      <div className="dots-icon"><BiDotsHorizontalRounded /></div>
-      <div className="dropdown">Ethereum</div>
-      <button>Connect</button>
-    </div>
-  </div>;
+  );
 };
 
 export default Header;
