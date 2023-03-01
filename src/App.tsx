f5beb640ea6e720804cbb5aaa7da1521e2276ff1
benqSzaw <45177027+benqSzaw@users.noreply.github.com>
2023-03-01 10:47:02 +0100

    packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import { WagmiConfig } from "wagmi";
-import client from "./blockchain/WagmiClient";
-import Profile from "./blockchain/Profile";
-import SwitchNetwork from "./blockchain/SwitchNetwork";
-import GetTotalSupply from "./blockchain/contract functions/GetTotalSupply";
-import Mint from "./blockchain/contract functions/Mint";
-
 import "./index.scss";
 
 const App = () => {
   return (
     <div>
-      <WagmiConfig client={client}>
-        <Profile />
-        <SwitchNetwork />
-        <GetTotalSupply />
-        <Mint />
-      </WagmiConfig>
+
     </div>
   );
 }
