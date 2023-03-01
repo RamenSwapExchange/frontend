85cb91e67b010a9a7a90b4ec97c726194c56a86c
grekuu <91375853+grekuu@users.noreply.github.com>
2023-03-01 10:38:08 +0100

    dropdown functionality

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -17,11 +17,18 @@ const Header = () => {
         <a>Pool</a>
       </div>
       <div className="header-mid">
-        <input type="text" placeholder="Search tokens and NFT collections" />
+        <input type="text" placeholder="Search tokens" />    
       </div>
       <div className="header-right">
-        <BiDotsHorizontalRounded className="dots-icon" />
         <div className="dropdown">
+          <BiDotsHorizontalRounded className="dots-icon" />
+          <ul className="dropdown-menu">
+            <li className="dropdown-item li">Vote in governance</li>
+            <li className="dropdown-item li">English</li>
+          </ul>
+        </div>
+
+        <div className="ethereum-dropdown">
           <FaEthereum />
           <span>Ethereum</span>
         </div>
