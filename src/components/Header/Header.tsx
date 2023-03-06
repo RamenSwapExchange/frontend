6573387690ac733f5618e1e78d0090de40cb6f06
grekuu <gracjanprusik@gmail.com>
2023-03-06 11:03:13 +0100

    dots dropdown finished

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import "./header.scss";
 import { MdOutlineRamenDining } from "react-icons/md";
 import { BiDotsHorizontalRounded } from "react-icons/bi";
-import { FaEthereum, FaScroll } from "react-icons/fa";
+import { FaScroll } from "react-icons/fa";
 import {
   AiOutlineSearch,
   AiOutlineTwitter,
@@ -10,6 +10,11 @@ import {
 import { SiDiscord } from "react-icons/si";
 import { useState } from "react";
 import { FiBarChart } from "react-icons/fi";
+import ethereumIcon from "/images/Ethereum.png";
+import polygonIcon from "/images/Polygon.svg";
+import optymismIcon from "/images/Optymism.svg";
+import arbitrumIcon from "/images/Arbitrum.svg";
+import celoIcon from "/images/Celo.svg";
 
 const Header = () => {
   const [dotsDropdownActive, setDotsDropdownActive] = useState<boolean>(false);
@@ -80,8 +85,14 @@ const Header = () => {
         </div>
 
         <div className="dropdown" onClick={ethereumClicked}>
-          <FaEthereum />
-          <span>Ethereum</span>
+          <div className="ethereum-dropdown-box">
+            <img
+              src={ethereumIcon}
+              alt="ethereum-icon"
+              className="ethereum-icon"
+            />
+            <span>Ethereum</span>
+          </div>
           <ul
             className={
               ethereumDropdownActive
@@ -89,11 +100,42 @@ const Header = () => {
                 : "dropdown-menu"
             }
           >
-            <li className="dropdown-item li">Ethereum</li>
-            <li className="dropdown-item li">Polygon</li>
-            <li className="dropdown-item li">Optimism</li>
-            <li className="dropdown-item li">Arbitrum</li>
-            <li className="dropdown-item li">Celo</li>
+            <li className="dropdown-item li li-hover">
+              <img
+                src={ethereumIcon}
+                alt="ethereum-icon"
+                className="ethereum-icon"
+              />
+              Ethereum
+            </li>
+            <li className="dropdown-item li li-hover">
+              <img
+                src={polygonIcon}
+                alt="polygon-icon"
+                className="ethereum-icon"
+              />
+              Polygon
+            </li>
+            <li className="dropdown-item li li-hover">
+              <img
+                src={optymismIcon}
+                alt="optymism-icon"
+                className="ethereum-icon"
+              />
+              Optimism
+            </li>
+            <li className="dropdown-item li li-hover">
+              <img
+                src={arbitrumIcon}
+                alt="arbitrum-icon"
+                className="ethereum-icon"
+              />
+              Arbitrum
+            </li>
+            <li className="dropdown-item li li-hover">
+              <img src={celoIcon} alt="celo-icon" className="ethereum-icon" />
+              Celo
+            </li>
           </ul>
         </div>
 
