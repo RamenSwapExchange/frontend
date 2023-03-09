688a3080c599d15f29de9101aea7ae82f625e791
grekuu <91375853+grekuu@users.noreply.github.com>
2023-03-09 15:29:55 +0100

    swap component updates

--- src/components/Swap/Swap.tsx
+++ src/components/Swap/Swap.tsx
@@ -1,8 +1,18 @@
 import "./swap.scss";
+import { FiSettings } from "react-icons/fi"
 
 const Swap = () => {
   return <div className="swap-container">
-    <div className="swap-box"></div>
+    <div className="swap-box">
+      <div className="swap-top">
+        <div>Swap</div>
+        <div><FiSettings className="settings-icon" /></div>
+      </div>
+      <div className="single-swap">
+        <input type="text" placeholder="0" className="swap-input" />
+      </div>
+
+    </div>
   </div>;
 };
 
