96ed47eccf75efe161ea36022217c563cf44f681
benqSzaw <45177027+benqSzaw@users.noreply.github.com>
2023-03-10 19:07:00 +0100

    feat: disconnect with small amount css

--- src/components/Header/ConnectButton.tsx
+++ src/components/Header/ConnectButton.tsx
@@ -5,24 +5,49 @@ import { useAppDispatch } from "../../redux/hooks";
 import { showPopUp } from "../../redux/appSlice";
 
 import { useAccount } from "wagmi";
+import { useDisconnect } from "wagmi";
+import { useState } from "react";
 
 const ConnectButton = () => {
+  const [isDropdown, setDropdown] = useState<boolean>(false);
   const dispatch = useAppDispatch();
+
   const { address } = useAccount();
+  const { disconnect } = useDisconnect();
 
   return (
     <>
-      {address ? (
-        <button>
-          {address.replace(address.substring(7, address.length - 5), "...")}
-        </button>
-      ) : (
+      <div className="button-with-dropdown">
         <div className="button-connect">
-          <div className="button-text" onClick={() => dispatch(showPopUp(true))}>Connect</div>
-          <div className="button-line"/>
-          <img className="button-arrowDown" src={arrowDown} />
+          {address ? (
+            <>
+              {address.replace(address.substring(7, address.length - 5), "...")}
+            </>
+          ) : (
+            <>
+              <div
+                className="button-text"
+                onClick={() => dispatch(showPopUp(true))}
+              >
+                Connect
+              </div>
+            </>
+          )}
+          <div className="button-line" />
+          <img
+            className="button-arrowDown"
+            src={arrowDown}
+            onClick={() => setDropdown(!isDropdown)}
+          />
         </div>
-      )}
+        {isDropdown && (
+          <div className="dropdown-connect">
+            dropdown
+            <br />
+            {address && <div onClick={() => disconnect()}>Disconnect</div>}
+          </div>
+        )}
+      </div>
     </>
   );
 };
