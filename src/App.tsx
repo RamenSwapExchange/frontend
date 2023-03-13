f6b141ac99203c86f34b22ae58cf55bea781547b
grekuu <91375853+grekuu@users.noreply.github.com>
2023-03-13 14:43:38 +0100

    root background color

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Tokens from "./components/Tokens/Tokens";
 import Swap from "./components/Swap/Swap";
 import Pool from "./components/Pool/Pool";
 import Header from "./components/Header/Header";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { selectPopUp } from "./redux/appSlice";
 import { useAppSelector } from "./redux/hooks";
 import ConnectPopUp from "./components/Header/ConnectPopUp";
--- src/components/Tokens/Tokens.tsx
+++ src/components/Tokens/Tokens.tsx
@@ -1,7 +1,7 @@
 import "./tokens.scss";
 
 const Tokens = () => {
-  return <div>Tokens</div>;
+  return <div className="container-sm tokens-container">Tokens <br />ber</div>;
 };
 
 export default Tokens;
