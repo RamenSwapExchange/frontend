d5b796b73d0061ca36d6a177a663295dfe93df8c
grekuu <91375853+grekuu@users.noreply.github.com>
2023-03-30 16:27:31 +0200

    header css update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import "./header.scss";
 import { MdOutlineRamenDining } from "react-icons/md";
 import { BiDotsHorizontalRounded } from "react-icons/bi";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -12,26 +13,36 @@ const Header = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link>Swap</Nav.Link>
-                    <Nav.Link>Tokens</Nav.Link>
-                    <Nav.Link>Pool</Nav.Link>
-                    <NavDropdown title={<BiDotsHorizontalRounded />} id="basic-nav-dropdown">
-                        <NavDropdown.Item>Vote in governance</NavDropdown.Item>
-                        <NavDropdown.Item>
-                            View more analytics
-                        </NavDropdown.Item>
-                        <NavDropdown.Item>Help center</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item>
-                            Documentation
-                        </NavDropdown.Item>
-                        <NavDropdown.Item>
-                            Feedback
-                        </NavDropdown.Item>
-                        <NavDropdown.Item>
-                            Legal & Privacy
-                        </NavDropdown.Item>
-                    </NavDropdown>
+                    <div className="header-left">
+                        <Link to="/swap" className="nav-link">
+                            <Nav.Link>Swap</Nav.Link>
+                        </Link>
+                        <Link to="/tokens" className="nav-link">
+                            <Nav.Link>Tokens</Nav.Link>
+                        </Link>
+                        <Link to="/pools" className="nav-link">
+                            <Nav.Link>Pools</Nav.Link>
+                        </Link>
+                        <NavDropdown title={<BiDotsHorizontalRounded className='dots-icon' />} id="basic-nav-dropdown">
+                            <NavDropdown.Item>Vote in governance</NavDropdown.Item>
+                            <NavDropdown.Item>
+                                View more analytics
+                            </NavDropdown.Item>
+                            <NavDropdown.Item>Help center</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item>
+                                Documentation
+                            </NavDropdown.Item>
+                            <NavDropdown.Item>
+                                Feedback
+                            </NavDropdown.Item>
+                            <NavDropdown.Item>
+                                Legal & Privacy
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </div>
+                    <div className="header-middle"></div>
+                    <div className="header-right"></div>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
