7107c46dd777b8d9e8d513a30ef1e738cf7e8b7a
benqSzaw <45177027+benqSzaw@users.noreply.github.com>
2023-03-31 08:39:43 +0200

    connectButton cd...

--- src/components/Header/ConnectButton.tsx
+++ src/components/Header/ConnectButton.tsx
@@ -1,13 +1,27 @@
 import "./ConnectButton.scss";
 
-import { useAccount } from "wagmi";
+import { useAccount, useConnect } from "wagmi";
 import { useDisconnect } from "wagmi";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 
 const ConnectButton = () => {
   const { address, isConnected } = useAccount();
   const { disconnect } = useDisconnect();
+  const { connect, error, isLoading } = useConnect({
+    connector: new MetaMaskConnector({}),
+  });
 
-  return <>{isConnected ? { address } : "Connect"}</>;
+  return (
+    <div
+      onClick={() => {
+        connect();
+      }}
+    >
+      {isConnected
+        ? address?.replace(address.substring(7, address.length - 5), "...")
+        : "Connect"}
+    </div>
+  );
 };
 
 export default ConnectButton;
