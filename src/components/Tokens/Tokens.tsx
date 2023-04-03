f6286e69bfcadd3f15bc9828ee2f338f33a83a5d
grekuu <91375853+grekuu@users.noreply.github.com>
2023-04-03 14:53:21 +0200

    filter section finished

--- src/components/Tokens/Tokens.tsx
+++ src/components/Tokens/Tokens.tsx
@@ -1,8 +1,36 @@
-import "./tokens.scss"
+import "./tokens.scss";
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const Tokens = () => {
     return (
-        <div>Tokens</div>
+        <div className="container-sm tokens-container">
+            <div className="tokens-title">Top tokens on Ramenswap</div>
+            <div className="filter-section">
+                <Dropdown>
+                    <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                        Ethereum
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item>A</Dropdown.Item>
+                        <Dropdown.Item>B</Dropdown.Item>
+                        <Dropdown.Item>C</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+
+                <Dropdown>
+                    <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                        1D
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item>A</Dropdown.Item>
+                        <Dropdown.Item>B</Dropdown.Item>
+                        <Dropdown.Item>C</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+
+                <input type="text" placeholder="Filter Tokens" />
+            </div>
+        </div>
     )
 }
 
