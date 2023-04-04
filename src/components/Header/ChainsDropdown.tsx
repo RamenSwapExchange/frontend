64249ea6c3a9c7fc32ad5ab7b11e33038951944f
benqSzaw <45177027+benqSzaw@users.noreply.github.com>
2023-04-04 09:17:53 +0200

    TODO: chain dropdown getting cancer

--- src/components/Header/ChainsDropdown.tsx
+++ src/components/Header/ChainsDropdown.tsx
@@ -2,6 +2,7 @@ import "./ChainsDropdown.scss";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { chainsWithIcons, getChainIcon } from "../../blockchain/WagmiClient";
 import { useAccount, useNetwork, useSwitchNetwork } from "wagmi";
+import { useState } from "react";
 
 const ChainsDropdown = () => {
   const { isError, isLoading, pendingChainId, switchNetwork } =
@@ -11,19 +12,27 @@ const ChainsDropdown = () => {
     });
   const { chain } = useNetwork();
   const { isConnected } = useAccount();
+  const [icon, setIcon] = useState<string>(getChainIcon(1));
+
+  const ChangeChain = (chainId: number) => {
+    setIcon(getChainIcon(chainId));
+
+    if (isConnected) switchNetwork!(chainId);
+  };
 
   const TitleDropdown = (
-    <div className={isLoading ? "chain-button chain-button-loading" : "chain-button"}>
+    <div
+      className={
+        isLoading ? "chain-button chain-button-loading" : "chain-button"
+      }
+    >
       {isLoading ? (
         <>
-          <div className="loader"/>
+          <div className="loader" />
           <img className="chain-icon" src={getChainIcon(pendingChainId!)} />
         </>
       ) : (
-        <img
-          className="chain-icon"
-          src={isConnected ? getChainIcon(chain?.id!) : getChainIcon(1)}
-        />
+        <img className="chain-icon" src={isConnected ? icon : getChainIcon(chain)} />
       )}
     </div>
   );
@@ -40,7 +49,7 @@ const ChainsDropdown = () => {
           <NavDropdown.Item
             key={id}
             className="chain-item-div"
-            onClick={() => switchNetwork!(chainMap.id)}
+            onClick={() => ChangeChain(chainMap.id)}
           >
             <img className="chain-icon" src={getChainIcon(chainMap.id)} />
             <div>{chainMap.name}</div>
