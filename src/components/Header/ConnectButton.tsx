c4146ee209fbe311d802f7a99d245f634b56e932
benqSzaw <45177027+benqSzaw@users.noreply.github.com>
2023-04-07 14:29:16 +0200

    add: theme, style, disconnect to offcanvas

--- src/components/Header/ConnectButton.tsx
+++ src/components/Header/ConnectButton.tsx
@@ -1,16 +1,15 @@
 import "./ConnectButton.scss";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import metaMaskLogo from "/images/MetaMaskLogo.svg";
 
 import { useAccount, useConnect } from "wagmi";
 import { useDisconnect } from "wagmi";
-import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 import { useState } from "react";
 
 const ConnectButton = () => {
   const { address, isConnected } = useAccount();
   const { disconnect } = useDisconnect();
   const { connect, connectors, error, isLoading } = useConnect({
-    connector: new MetaMaskConnector({}),
     onSuccess() {
       handleClose();
     },
@@ -19,6 +18,8 @@ const ConnectButton = () => {
   const [showCanvas, setShowCanvas] = useState(false);
   const handleClose = () => setShowCanvas(false);
   const handleShow = () => setShowCanvas(true);
+
+  const [theme, setTheme] = useState("light");
 
   return (
     <>
@@ -38,28 +39,57 @@ const ConnectButton = () => {
           <div className="left-panel" onClick={handleClose}>
             &gt;&gt;
           </div>
+
           <div className="right-panel">
-            <div className="headear">
-              <h4> Connect a wallet </h4>
-              <button> settings </button>
+            {isConnected ? (
+              <div onClick={() => disconnect()}>Disconnect</div>
+            ) : (
+              <div>
+                <div className="connector-header">Connect a wallet</div>
+                <div className="connectors">
+                  {connectors.map((connector) => {
+                    return (
+                      <div
+                        key={connector.id}
+                        className="connector-div"
+                        onClick={() => {
+                          connect({ connector });
+                        }}
+                      >
+                        <img
+                          src={connector.name == "MetaMask" ? metaMaskLogo : ""}
+                          className="connector-logo"
+                        />
+                        <div className="connector-name">{connector.name}</div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+
+            <div className="theme-div">
+              <div className="theme-headear">Theme:</div>
+              <div className="theme-buttons">
+                <div
+                  className={theme == "light" ? "active-theme" : ""}
+                  onClick={() => {
+                    setTheme("light");
+                  }}
+                >
+                  light
+                </div>
+                <div
+                  className={theme == "dark" ? "active-theme" : ""}
+                  onClick={() => {
+                    setTheme("dark");
+                  }}
+                >
+                  dark
+                </div>
+              </div>
             </div>
-            <div>
-              {connectors.map((connector) => {
-                return (
-                  <div
-                    key={connector.id}
-                    onClick={() => {
-                      connect({ connector });
-                    }}
-                  >
-                    {connector.name}
-                  </div>
-                );
-              })}
-            </div>
-            <div>
-              <div>Authors: qpa</div>
-            </div>
+            
           </div>
         </div>
       </Offcanvas>
