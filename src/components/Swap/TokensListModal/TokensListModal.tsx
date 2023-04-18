f43bc55b8262dbaf63068eaeef60769265382515
grekuu <91375853+grekuu@users.noreply.github.com>
2023-04-18 08:26:42 +0200

    tokens loading on scroll

--- src/components/Swap/TokensListModal/TokensListModal.tsx
+++ src/components/Swap/TokensListModal/TokensListModal.tsx
@@ -2,80 +2,80 @@ import { useEffect, useRef, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import {
-  changePage,
-  selectModal,
-  selectTokens,
-  showModal,
-  TokensType,
+    changePage,
+    selectModal,
+    selectTokens,
+    showModal,
+    TokensType,
 } from "../../../redux/appSlice";
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
 import "./tokensListModal.scss";
 
 const TokensListModal = () => {
-  const [filteredTokens, setFilteredTokens] = useState<TokensType[]>();
-  const [tokensFilter, setTokensFilter] = useState("");
+    const [filteredTokens, setFilteredTokens] = useState<TokensType[]>();
+    const [tokensFilter, setTokensFilter] = useState("");
 
-  const dispatch = useAppDispatch();
-  const show = useAppSelector(selectModal);
-  const tokens = useAppSelector(selectTokens);
+    const dispatch = useAppDispatch();
+    const show = useAppSelector(selectModal);
+    const tokens = useAppSelector(selectTokens);
 
-  console.log(tokens);
+    console.log(tokens);
 
-  const handleClose = () => dispatch(showModal(false));
-  const [page, setPage] = useState(0);
-  const boxRef = useRef<HTMLDivElement>(null);
+    const handleClose = () => dispatch(showModal(false));
+    const [page, setPage] = useState(0);
+    const boxRef = useRef<HTMLDivElement>(null);
 
-  const handleScroll = () => {
-    const box = boxRef.current;
-    if (box && box.scrollTop + box.clientHeight === box.scrollHeight) {
-      setPage((prevPage) => prevPage + 1);
-    }
-  };
+    const handleScroll = () => {
+        const box = boxRef.current;
+        if (box && box.scrollTop + box.clientHeight === box.scrollHeight) {
+            setPage((prevPage) => prevPage + 1);
+        }
+    };
 
-  useEffect(() => {
-    console.log("page: " + page);
-    dispatch(changePage(page));
-  }, [page, dispatch]);
+    useEffect(() => {
+        console.log("page: " + page);
+        dispatch(changePage(page));
+    }, [page, dispatch]);
 
-  useEffect(() => {
-    let filteredArray = tokens.filter((token) =>
-      token.name.toLowerCase().includes(tokensFilter.toLowerCase())
+    useEffect(() => {
+        let filteredArray = tokens.filter((token) =>
+            token.name.toLowerCase().includes(tokensFilter.toLowerCase())
+        );
+        setFilteredTokens(oldList => oldList ? [...oldList, ...filteredArray] : filteredArray);
+    }, [show, tokensFilter, page]);
+
+    return (
+        <Modal show={show} onHide={handleClose} className="tokens-modal">
+            <Modal.Header closeButton>
+                <Modal.Title>Select a token</Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="tokens-modal-body">
+                <input
+                    type="text"
+                    placeholder="Search name"
+                    className="search-token-input"
+                    onChange={(e) => setTokensFilter(e.target.value)}
+                />
+                <div className="tokens-list" onScroll={handleScroll} ref={boxRef}>
+                    {filteredTokens?.map((token) => (
+                        <div key={token.key} className="single-token">
+                            <div className="token-image">
+                                {token.images ? (
+                                    <img src={token.images[1]}></img>
+                                ) : (
+                                    <img src={token.image}></img>
+                                )}
+                            </div>
+                            <div>
+                                <div className="token-name">{token.name}</div>
+                                <div className="token-symbol">{token.symbol}</div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </Modal.Body>
+        </Modal>
     );
-    setFilteredTokens(filteredArray);
-  }, [show, tokensFilter]);
-
-  return (
-    <Modal show={show} onHide={handleClose} className="tokens-modal">
-      <Modal.Header closeButton>
-        <Modal.Title>Select a token</Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="tokens-modal-body">
-        <input
-          type="text"
-          placeholder="Search name"
-          className="search-token-input"
-          onChange={(e) => setTokensFilter(e.target.value)}
-        />
-        <div className="tokens-list" onScroll={handleScroll} ref={boxRef}>
-          {filteredTokens?.map((token) => (
-            <div key={token.key} className="single-token">
-              <div className="token-image">
-                {token.images ? (
-                  <img src={token.images[1]}></img>
-                ) : (
-                  <img src={token.image}></img>
-                )}
-              </div>
-              <div>
-                <div className="token-name">{token.name}</div>
-                <div className="token-symbol">{token.symbol}</div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </Modal.Body>
-    </Modal>
-  );
 };
 
 export default TokensListModal;
