5524d7d692a193f65fdfd302162b004f4e75d409
benqSzaw <45177027+benqSzaw@users.noreply.github.com>
2023-04-26 23:01:26 +0200

    add swap to homepage

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,3 +1,4 @@
+import Swap from "../Swap/Swap"
 import "./home.scss"
 import logo from "/logo.png"
 
@@ -6,6 +7,9 @@ import { Link } from "react-router-dom"
 const Home = () => {
     return (
         <div className='home-container'>
+            <div className="home-swap">
+                <Swap />
+            </div>
             <img src={logo} alt="logo" className="home-logo" />
             <div className="home-header"> Ramen Swap </div>
             <Link style={{ textDecoration: "none" }} to="/swap">
