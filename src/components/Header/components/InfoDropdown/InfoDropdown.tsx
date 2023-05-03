22420bf0aecd0cec2b280395ca5effd2bd6e6f46
benqSzaw <45177027+benqSzaw@users.noreply.github.com>
2023-05-03 02:38:39 +0200

    fix(infoDropdown, header): dots icon

--- src/components/Header/components/InfoDropdown/InfoDropdown.tsx
+++ src/components/Header/components/InfoDropdown/InfoDropdown.tsx
@@ -1,5 +1,5 @@
-import { NavDropdown } from 'react-bootstrap'
 import './infoDropdown.scss'
+import { NavDropdown } from 'react-bootstrap'
 import { BiDotsHorizontalRounded } from 'react-icons/bi'
 import { FaScroll } from 'react-icons/fa'
 import { FiBarChart } from 'react-icons/fi'
@@ -7,24 +7,26 @@ import { AiFillGithub, AiOutlineTwitter } from 'react-icons/ai'
 
 const InfoDropdown = () => {
     return (
-        <NavDropdown title={<BiDotsHorizontalRounded className="dots-icon" />} id="basic-nav-dropdown">
-            <NavDropdown.Item className="dropdown-li">
-                <FaScroll className="dropdown-icon" />
-                Vote in governance
-            </NavDropdown.Item>
-            <NavDropdown.Item className="dropdown-li">
-                <FiBarChart className="dropdown-icon" />
-                View more analytics
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item className="info-item">Help center &#8599;</NavDropdown.Item>
-            <NavDropdown.Item className="info-item">Documentation &#8599;</NavDropdown.Item>
-            <NavDropdown.Item className="info-item">Feedback &#8599;</NavDropdown.Item>
-            <NavDropdown.Item className="info-item">Legal & Privacy &#8599;</NavDropdown.Item>
-            <NavDropdown.ItemText className="dropdown-socials">
-                <AiOutlineTwitter className="social-link" />
-                <AiFillGithub className="social-link" />
-            </NavDropdown.ItemText>
+        <NavDropdown title={<BiDotsHorizontalRounded className="dots-icon" />} id="basic-nav-dropdown" className='info-dropdown-container'>
+            <div className='mobile-container'>
+                <NavDropdown.Item className="dropdown-li">
+                    <FaScroll className="dropdown-icon" />
+                    Vote in governance
+                </NavDropdown.Item>
+                <NavDropdown.Item className="dropdown-li">
+                    <FiBarChart className="dropdown-icon" />
+                    View more analytics
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item className="info-item">Help center &#8599;</NavDropdown.Item>
+                <NavDropdown.Item className="info-item">Documentation &#8599;</NavDropdown.Item>
+                <NavDropdown.Item className="info-item">Feedback &#8599;</NavDropdown.Item>
+                <NavDropdown.Item className="info-item">Legal & Privacy &#8599;</NavDropdown.Item>
+                <NavDropdown.ItemText className="dropdown-socials">
+                    <AiOutlineTwitter className="social-link" />
+                    <AiFillGithub className="social-link" />
+                </NavDropdown.ItemText>
+            </div>
         </NavDropdown>
     )
 }
