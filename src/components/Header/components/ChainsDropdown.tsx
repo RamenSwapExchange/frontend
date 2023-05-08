0c002a754c005707ffee0d79b719168702c92ad3
grekuu <gracjanprusik@gmail.com>
2023-05-08 16:59:21 +0200

    feat(ChainsDropdown): body styles load in component

--- src/components/Header/components/ChainsDropdown.tsx
+++ src/components/Header/components/ChainsDropdown.tsx
@@ -2,28 +2,28 @@ import './chainsDropdown.scss'
 import { NavDropdown, OverlayTrigger } from 'react-bootstrap'
 import { BiError } from 'react-icons/bi'
 import { RiArrowDropDownLine, RiArrowDropUpLine } from 'react-icons/ri'
-import errorIcon from "/error.png"
+import errorIcon from '/error.png'
 
 import { useAccount } from 'wagmi'
 import { watchNetwork } from '@wagmi/core'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import useNet from '../../../common/useNet'
 import useNetIcon from '../../../common/useNetIcon'
 
 const ChainsDropdown = () => {
-    const resetErrorTime = 15 * 1000; //seconds
+    const resetErrorTime = 15 * 1000 //seconds
 
     const { netId, nets, unsupported, changeOfflineNet, changeNet } = useNet({
         onErrorChangeNet() {
             setIsError(true)
             setTimeout(() => {
                 setIsError(false)
-            }, resetErrorTime);
-        }
+            }, resetErrorTime)
+        },
     })
-    const { isLoading, pendingChainId, switchNetwork } = changeNet;
-    const { getIcon } = useNetIcon();
+    const { isLoading, pendingChainId, switchNetwork } = changeNet
+    const { getIcon } = useNetIcon()
 
     const { isConnected } = useAccount({
         onDisconnect() {
@@ -35,7 +35,7 @@ const ChainsDropdown = () => {
     })
 
     const [isArrowUp, setIsArrowUp] = useState(false)
-    const [isError, setIsError] = useState(false);
+    const [isError, setIsError] = useState(false)
 
     //fires when chain changed
     watchNetwork(() => {
@@ -60,10 +60,7 @@ const ChainsDropdown = () => {
             {isError && <BiError />}
             {isLoading && <div className="loader" />}
 
-            <img
-                className="chain-icon"
-                src={getIcon(isLoading ? pendingChainId! : netId)}
-            />
+            <img className="chain-icon" src={getIcon(isLoading ? pendingChainId! : netId)} />
             {isArrowUp ? <RiArrowDropUpLine fontSize={20} /> : <RiArrowDropDownLine fontSize={20} />}
         </div>
     )
@@ -78,6 +75,13 @@ const ChainsDropdown = () => {
             </OverlayTrigger>
         )
     }
+
+    useEffect(() => {
+        document.body.style.background = 'linear-gradient(#e0e7ff, #ffffff)'
+        document.body.style.height = '100%'
+        document.body.style.backgroundSize = 'cover'
+        document.body.style.backgroundRepeat = 'no-repeat'
+    }, [])
 
     return (
         <>
@@ -99,16 +103,21 @@ const ChainsDropdown = () => {
                 })}
             </NavDropdown>
 
-            {isError &&
-                <div className='chains-error-container'>
-                    <img src={errorIcon} alt="error" className='error-icon' />
-                    <div className='error-text'>
+            {isError && (
+                <div className="chains-error-container">
+                    <img src={errorIcon} alt="error" className="error-icon" />
+                    <div className="error-text">
                         Failed to switch networks.
                         <br /> Check MetaMask.
                     </div>
-                    <button type="button" onClick={() => setIsError(false)} className="btn-close" aria-label="Close"></button>
+                    <button
+                        type="button"
+                        onClick={() => setIsError(false)}
+                        className="btn-close"
+                        aria-label="Close"
+                    ></button>
                 </div>
-            }
+            )}
         </>
     )
 }
