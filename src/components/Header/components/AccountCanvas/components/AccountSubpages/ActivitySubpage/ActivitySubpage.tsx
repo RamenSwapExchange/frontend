5bd8f5c8aebe2f362f0dd05ddd35cd9fce204fc1
grekuu <gracjanprusik@gmail.com>
2023-05-14 18:47:28 +0200

    feat(ActivitySubpage): component finished

--- src/components/Header/components/AccountCanvas/components/AccountSubpages/ActivitySubpage/ActivitySubpage.tsx
+++ src/components/Header/components/AccountCanvas/components/AccountSubpages/ActivitySubpage/ActivitySubpage.tsx
@@ -1,7 +1,14 @@
-import './activitySubpage.scss'
+import '../AccountSubpage/accountSubpage.scss'
+import activityImg from '/subpages/activity-subpage.png'
 
 const ActivitySubpage = () => {
-    return <div>ActivitySubpage</div>
+    return (
+        <div className="subpage-container">
+            <img src={activityImg} alt={activityImg} />
+            <h5>No activity yet</h5>
+            <p>Your onchain transactions and crypto purchases will appear here.</p>
+        </div>
+    )
 }
 
 export default ActivitySubpage
